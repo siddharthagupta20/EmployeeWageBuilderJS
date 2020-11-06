5dbdcb1c1a43397cb016f63f36341d9325f90aed
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-11-06 10:13:55 +0530

    [Siddhartha] created: emp wage problem with javascript

---
+++ empWage.js
@@ -0,0 +1,8 @@
+const IS_ABSENT= 1;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck == IS_ABSENT){
+    console.log("UC1-Emp is absent.");
+}
+else{
+    console.log("UCI-Emp is present");
+}
