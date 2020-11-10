ee1b36cde4f16f50b92fdf66609d88fa2134a799
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-11-11 00:02:52 +0530

    [Siddhartha] refactored:using regex

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -5,15 +5,39 @@ class EmployeePayrollData{
     startDate;
 
     constructor(...params){
-        this.id = params[0];
+        let idAndSalaryRegex = RegExp("[0-9]+");
+        if(idAndSalaryRegex.test(params[0])){
+        this.id = params[0];}
+        else{
+            throw "ID is incorrect";
+        }
         this.name = params[1];
-        this.salary= params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+        if(idAndSalaryRegex.test(params[2])){
+        this.salary= params[2];}
+        else{
+            throw "Salary is incorrect";
+        }
+        let genderRegex = RegExp("[MF]");
+        if(genderRegex.test(params[3])){
+        this.gender = params[3];}
+        else{
+            throw "Gender is incorrect";
+        }
+        let dateRegex = RegExp("[0-2][0-9][0-9][0-9][-]");
+        if(params[4] <= new Date()){
+        this.startDate = params[4];}
+        else{
+            throw "Date is incorrect";
+        }
     }
 
     get name(){ return this._name;}
-    set name(name){this._name = name;}
+    set name(name){
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw "Name is Incorrect";
+    }
 
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
@@ -23,10 +47,5 @@ class EmployeePayrollData{
                 ", gender="+this.gender+", startDate="+empDate;
     }
 }
-
-let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.name = "john";
-console.log(employeePayrollData.toString());
 let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayrollData);
