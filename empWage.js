9a04004557eb78e0a4f5b0a7032dfb3a6e7f3767
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-11-10 23:35:16 +0530

    [Siddhartha] refactored:added function for employee hours

--- empWage.js
+++ empWage.js
@@ -1,34 +1,21 @@
-//U1
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-{const IS_ABSENT= 1;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck == IS_ABSENT){
-    console.log("UC1-Emp is absent.");
-}
-else{
-    console.log("UC1-Emp is present");
-}
-}
-
-//UC2
-{const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HRS=4;
-const FULL_TIME_HRS=8;
-const WAGE_PER_HOUR=20;
-
-let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
+function getWorkHours(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
-        empHrs=PART_TIME_HRS;
-        break;
+        return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs=FULL_TIME_HRS;
-        break;
+        return FULL_TIME_HOURS;
     default:
-        empHrs=0;
+        return 0;
 }
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log(empWage);
 }
+empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: "+empWage);
